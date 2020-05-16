b4efc4bbba4fcf8c8c2ad5b2d83e775a46f5b437
eugene <eugene.voskoley@advance-auto.com>
2020-05-16 15:43:36 -0400

    - started kanban board - added bootstrap - created columns

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+  const columns = [
+      'todo',
+      'progress',
+      'review',
+      'done'
+  ];
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+        <h1>Board</h1>
+        <div className="row">
+            {
+                columns.map(col => (
+                    <div className="col-sm d-flex">
+                    <div className="border w-100">
+                        <h3>{col}</h3>
+                    </div>
+                </div>
+                ))
+            }
+
+        </div>
     </div>
+
   );
 }
 
